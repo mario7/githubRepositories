9e4a69cf87b53e8ed893bfe8f34bd028fad7823c
snowman <from21cjp@yahoo.co.jp>
2017-10-12 00:19:52 +0900

    add article class

---
+++ RxSwift/documentAppSample/documentAppSample/Article.swift
@@ -0,0 +1,41 @@
+//
+//  Article.swift
+//  documentAppSample
+//
+//  Created by snowman on 2017/10/11.
+//  Copyright © 2017年 snowman. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+class ArticleViewModel {
+    var title = Variable<String>("Titleですよ")
+    var content = Variable<String>("Contentですよ")
+}
+
+class ArticleViewController: UIViewController {
+    
+    @IBOutlet weak var articleTitleLabel: UILabel!
+    @IBOutlet weak var articleContentLabel: UILabel!
+    
+    let viewModel = ArticleViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        
+        // Title
+        viewModel.title
+            .asObservable()
+            .bindTo(articleTitleLabel.rx_text)
+            .addDisposableTo(disposeBag)
+        
+        // Content
+        viewModel.content
+            .asObservable()
+            .bindTo(articleContentLabel.rx_text)
+            .addDisposableTo(disposeBag)
+    }
+}
+
